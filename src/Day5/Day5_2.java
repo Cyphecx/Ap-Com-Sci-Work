390c7c8cbc22302c6fe4ca0f2268891f90b8b663
WyattL <whauben11@gmail.com>
2017-09-11 13:27:56 -0400

    working on Day 5 #2

---
+++ src/Day5/Day5_2.java
@@ -0,0 +1,27 @@
+package src.Day5;
+
+import java.util.*;
+
+public class Day5_2 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many doubles are you entering?");
+        int size = scan.nextInt();
+        double[] dubs = new double[size];
+        for(int i = 0; i < size; i++){
+            dubs[i] = scan.nextDouble();
+        }
+        for (double x : dubs) {
+            System.out.println(x);
+        }
+    }
+
+    public static void reverseArray(double[] x){
+        for(int i = 0; i < (x.length/2)+1; i++){
+            System.out.println(x[i]);
+            double temp = x[i];
+            x[i] = x[x.length-1-i];
+            x[x.length-1-i] = temp;
+        }
+    }
+}
