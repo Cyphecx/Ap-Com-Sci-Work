ccf2f02de9d7a80a09e90b53f312a8664bff822d
Cyphecx <whauben11@gmail.com>
2017-09-10 20:14:03 -0400

    Finished Day4_1

--- src/Day4/Day4_1.java
+++ src/Day4/Day4_1.java
@@ -1,7 +1,35 @@
 package Day4;
 
-public class Day4_1 {
-    public static void main(String[] args){
+import java.util.*;
 
+public class Day4_1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter six doubles.");
+        double[] dubs = new double[6];
+        for (int i = 0; i < dubs.length; i++) {
+            dubs[i] = scan.nextDouble();
+        }
+        System.out.println("true for ascending false for descending");
+        boolean order = scan.nextBoolean();
+        sortArray(dubs,order);
+        for(double x : dubs){
+            System.out.println(x);
+        }
+    }
+
+    private static void sortArray(double[] x, boolean y) {
+        if (y) {
+            Arrays.sort(x);
+        }
+        else {
+            for(int i = 0; i < x.length; i++){
+                x[i] = x[i] * -1;
+            }
+            Arrays.sort(x);
+            for(int i = 0; i < x.length; i++){
+                x[i] = x[i] * -1;
+            }
+        }
     }
 }
