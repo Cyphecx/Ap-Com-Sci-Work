89da0544d8272e5fd9131c1775a7620a4fd64df6
WyattL <whauben11@gmail.com>
2017-09-13 12:21:20 -0400

    fixed Day5_4

--- src/Day5/Day5_4.java
+++ src/Day5/Day5_4.java
@@ -5,24 +5,23 @@ import java.util.ArrayList;
 public class Day5_4 {
     public static void main(String[] args){
         ArrayList<Integer> factors = new ArrayList<Integer>();
-        int ctr = 20000;
-        while(factors.size() < 5){
+        int ctr = 40000;
+        while(factors.size() < 5) {
             ctr++;
-            System.out.println(ctr);
             factors.clear();
-            for(int i = 2; i < ctr; i++){
-                if(ctr % i == 0 && isPrime(i)){
+            for (int i = 2; i < ctr; i++) {
+                if (ctr % i == 0 && isPrime(i)) {
                     factors.add(i);
                 }
             }
-
         }
         System.out.println(ctr);
+        System.out.println(factors);
     }
 
     public static boolean isPrime(int in){
-        for(int i = 2; i < Math.sqrt(in); i++){
-            if(in % i ==0){
+        for(int i = 2; i < Math.sqrt(in)+1; i++){
+            if(in % i ==0 && i != in){
                 return false;
             }
         }
