3698ebf5345b43b63489aef2cde82108e4ed4a0d
WyattL <whauben11@gmail.com>
2017-09-14 01:21:35 -0400

    working on Day5_4

--- src/Day5/Day5_4.java
+++ src/Day5/Day5_4.java
@@ -5,26 +5,39 @@ import java.util.ArrayList;
 public class Day5_4 {
     public static void main(String[] args){
         ArrayList<Integer> factors = new ArrayList<Integer>();
-        int ctr = 40000;
-        while(factors.size() < 5) {
-            ctr++;
+        int product = 1;
+        int ctr;
+        for(ctr = 40000; ctr < Integer.MAX_VALUE; ctr++) {
             factors.clear();
-            for (int i = 2; i < ctr; i++) {
-                if (ctr % i == 0 && isPrime(i)) {
-                    factors.add(i);
-                }
+            product = 1;
+            factors = primeFactor(ctr);
+            for (int i: factors) {
+                product *= i;
+            }
+            System.out.println((factors.size() == 5 && product == ctr) + " " + ctr);
+            if(factors.size() == 5 && product == ctr){
+                break;
             }
         }
         System.out.println(ctr);
         System.out.println(factors);
+        int n = 1;
+        for (int i: factors) {
+            n *= i;
+        }
+        System.out.println(n);
     }
 
-    public static boolean isPrime(int in){
-        for(int i = 2; i < Math.sqrt(in)+1; i++){
-            if(in % i ==0 && i != in){
-                return false;
+    public static ArrayList<Integer> primeFactor(int in){
+        int m = in;
+        ArrayList<Integer> factors = new ArrayList<Integer>();
+        for(int i = 2; i < m; i++){
+            if(m % i == 0){
+                factors.add(i);
+                m /= i;
+                i--;
             }
         }
-        return true;
+        return factors;
     }
 }
