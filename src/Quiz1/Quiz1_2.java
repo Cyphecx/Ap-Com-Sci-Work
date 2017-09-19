2070ff6079b96284d70f0b084169e449a8f39f78
WyattL <whauben11@gmail.com>
2017-09-19 12:29:00 -0400

    First Quiz

---
+++ src/Quiz1/Quiz1_2.java
@@ -0,0 +1,12 @@
+package Quiz1;
+
+public class Quiz1_2 {
+    public static void main(String[] args){
+
+    }
+    public static void scramArray(int[] array){
+        for(int i = 0; i < 100; i++){
+            int rand1 = (int)(Math.random()*array.length);
+        }
+    }
+}
