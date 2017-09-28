49f4ed9203a3d9d101bed1b51f3281c0c27316b2
WyattL <whauben11@gmail.com>
2017-09-28 12:51:05 -0400

    Fixed MyMath

--- src/UnitTwo/Day5/MathTester.java
+++ src/UnitTwo/Day5/MathTester.java
@@ -4,8 +4,8 @@ public class MathTester {
     public static void main(String[] args) {
         System.out.println(MyMath.abs(1));
         System.out.println(MyMath.abs(-1));
-        System.out.println(MyMath.exponent(5,3));
-        System.out.println(MyMath.nearPerfect(58));
-        System.out.println(MyMath.round(5.23863,3));
+        System.out.println(MyMath.exponent(5,-2));
+        System.out.println(MyMath.nearPerfect(58)+"\n");
+        System.out.println(MyMath.round(5.123456789,8));
     }
 }
--- src/UnitTwo/Day5/MyMath.java
+++ src/UnitTwo/Day5/MyMath.java
@@ -11,10 +11,14 @@ public class MyMath {
     }
     public static double exponent(double base, int exp){
         int output = 1;
-        for(int i = 0; i <= exp; i++){
+        for(int i = 0; i < MyMath.abs(exp); i++){
             output *= base;
         }
-        return output;
+        if(exp < 0) {
+            return base/(output*base);
+        }
+
+            return output;
     }
     public static int nearPerfect(int in){
         if(in < 1){
@@ -45,11 +49,17 @@ public class MyMath {
         return i-1;
     }
     public static double round(double num, int decimals){
-        String con = String.valueOf(num);
+        String con = ""+num;
         if(con.length()-2 == decimals || con.length()-2 < decimals){
             return num;
         }
-        else if(Integer.parseInt(""+con.charAt(decimals+2)) < 5){
+        boolean greater = false;
+        for(int i = 5; i < 10; i++){
+            if(con.substring(decimals+2, decimals+3).equals(""+i)){
+                greater = true;
+            }
+        }
+        if(!greater){
             double temp = num;
             for(int i = 0; i < decimals; i++){
                 temp *= 10;
@@ -65,7 +75,8 @@ public class MyMath {
             for(int i = 0; i < decimals; i++){
                 temp *= 10;
             }
-            temp = (int)(temp)+1;
+            int temp2 = (int)(temp)+1;
+            temp = temp2;
             for(int i = 0; i < decimals; i++){
                 temp /= 10;
             }
