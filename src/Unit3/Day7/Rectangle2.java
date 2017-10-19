a24e56ee34e473419351784891f948abbe9dea4c
WyattL <whauben11@gmail.com>
2017-10-19 12:24:54 -0400

    Finished unit 3 day 7

--- src/Unit3/Day7/Rectangle2.java
+++ src/Unit3/Day7/Rectangle2.java
@@ -6,8 +6,8 @@ public class Rectangle2 {
     private static int numRectangles = 0;
     private static double largestArea = 0;
     private static double largestPerimeter = 0;
-    private static double length = 0;
-    private static double width = 0;
+    private double length = 0;
+    private double width = 0;
     private static int numSquares=0;
 
     Rectangle2(double x, double y) {
@@ -22,29 +22,71 @@ public class Rectangle2 {
         }
 
         this.numRectangles++;
-        double a = 1;
-        double b = (-1/2)*this.perimeter;
+        double a = 1.0;
+        double b = (-(1.0/2.0))*this.perimeter;
         double c = this.area;
-        double descriminent = b*b - 4*a*c;
-        if(descriminent == 0){
+        double discriminant = (b*b) - (4.0*a*c);
+        System.out.println(discriminant);
+        if(discriminant == 0){
             numSquares++;
+            this.length = (-b + Math.sqrt(discriminant)) / 2.0;
+            this.width = (-b + Math.sqrt(discriminant)) / 2.0;
+        }
+        else if(discriminant > 0){
+            this.length = (-b + Math.sqrt(discriminant)) / 2.0;
+            this.width = (-b - Math.sqrt(discriminant)) / 2.0;
+        }
+        else{
+            numRectangles--;
         }
     }
 
     double getSmall() {
-
+        if(this.length > this.width){
+            return this.width;
+        }
+        else{
+            return this.length;
+        }
     }
 
     double getLarge() {
+        if(this.length < this.width){
+            return this.width;
+        }
+        else{
+            return this.length;
+        }
+    }
 
+    public static double getLargestArea() {
+        return largestArea;
+    }
+
+    public static double getLargestPerimeter() {
+        return largestPerimeter;
+    }
+
+    public static int getNumRectangles() {
+        return numRectangles;
+    }
+
+    public static int getNumSquares() {
+        return numSquares;
     }
 
     static Rectangle2 longestSide(Rectangle2 [] rec) {
-
+        int indexOfLargestSide = 0;
+        for (int i = 0; i < rec.length; i++) {
+            if(rec[i].getLarge() > rec[indexOfLargestSide].getLarge()){
+                indexOfLargestSide = i;
+            }
+        }
+        return rec[indexOfLargestSide];
     }
 
     public String toString() {
-
+        return "Short side: "+ this.getSmall() +" Large side: "+ this.getLarge() +" Area: "+ this.area +" Perimeter: "+ this.perimeter;
     }
 
 }
--- src/Unit3/Day7/Tester.java
+++ src/Unit3/Day7/Tester.java
@@ -1,7 +1,24 @@
 package Unit3.Day7;
 
+        import Unit2.Day1.Rectangle;
+
+        import java.util.Scanner;
+
 public class Tester {
     public static void main(String[] args){
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many rectangles?");
+        Rectangle2[] rects = new Rectangle2[scan.nextInt()];
+        for (int i = 0; i < rects.length; i++) {
+            System.out.println("Enter the area and perimeter for Rectangle #"+ (i+1));
+            rects[i] = new Rectangle2(scan.nextDouble(), scan.nextDouble());
+        }
+        for (Rectangle2 r : rects) {
+            System.out.println(r);
+        }
+        System.out.println("There are " + Rectangle2.getNumRectangles() + " Rectangles");
+        System.out.println(Rectangle2.getNumSquares() + " of the Rectangles are Squares");
+        System.out.println("The longest side length is: "+ Rectangle2.longestSide(rects).getLarge());
+        System.out.println("The largest area is: "+ Rectangle2.getLargestArea());
     }
 }
